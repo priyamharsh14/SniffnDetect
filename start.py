b2c135a444148415de541600c52836eca68fd803
Priyam Harsh <priyamharsh14@gmail.com>
2020-10-04 13:22:11 +0530

    Created SniffnDetect class

--- start.py
+++ start.py
@@ -12,6 +12,120 @@ SniffnDetect v.1.1
 -----------------------
 '''
 
+class SniffnDetect():
+	def __init__(self):
+		self.INTERFACE = conf.iface
+		self.MY_IP = [x[4] for x in conf.route.routes if x[2] != '0.0.0.0' and x[3]==INTERFACE][0]
+		self.MY_MAC = get_if_hwaddr(INTERFACE)
+		self.MY_NETMASK = [IPv4Address(x[1]).compressed for x in conf.route.routes if x[3]==INTERFACE and x[4]==MY_IP and x[2]=='0.0.0.0' and IPv4Address(x[1]).compressed.startswith("255.") and IPv4Address(x[0]).compressed.startswith(MY_IP.split(".")[0]) and IPv4Address(x[0]).compressed.endswith(".0")][0]
+		self.PACKETS_QUEUE = Queue()
+		self.MAC_TABLE = {}
+		self.RECENT_ACTIVITIES = []
+		self.FILTERED_ACTIVITIES = {
+			'TCP-SYN': {'flag': False, 'activities': [], 'attacker-mac': []},
+			'TCP-SYNACK': {'flag': False, 'activities': [], 'attacker-mac': []},
+			'ICMP-POD': {'flag': False, 'activities': [], 'attacker-mac': []},
+			'ICMP-SMURF': {'flag': False, 'activities': [], 'attacker-mac': []},
+		}
+
+	def sniffer_threader(self):
+		while True:
+			pkt = sniff(count=1)
+			with threading.Lock():
+				self.PACKETS_QUEUE.put(pkt[0])
+
+	def analyze_threader(self):
+		while True:
+			pkt = self.PACKETS_QUEUE.get()
+			self.analyze_packet(pkt)
+			self.PACKETS_QUEUE.task_done()
+
+	def check_avg_time(self, activities):
+		time = 0
+		c = -1
+		while c>-21:
+			time += activities[c][0] - activities[c-1][0]
+			c -= 1
+		time /= len(activities)
+		return ( time<2 and self.RECENT_ACTIVITIES[-1][0] - activities[-1][0] < 10)
+
+	def find_attackers(self, mac_data):
+		msg = []
+		for mac in mac_data:
+			msg.append("["+str(self.MAC_TABLE[mac])+" ("+mac+")]" if mac in self.MAC_TABLE else "[Unknown IP ("+mac+")]")
+		return " ".join(msg)
+	
+	def set_flags(self):
+		for category in self.FILTERED_ACTIVITIES:
+			if len(self.FILTERED_ACTIVITIES[category]['activities'])>20:
+				self.FILTERED_ACTIVITIES[category]['flag'] = check_avg_time(self.FILTERED_ACTIVITIES[category]['activities'])
+				if self.FILTERED_ACTIVITIES[category]['flag']:
+					self.FILTERED_ACTIVITIES[category]['attacker-mac'] = list(set([i[3] for i in self.FILTERED_ACTIVITIES[category]['activities']]))
+	
+	def analyze_packet(self, pkt):
+		src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, tcp_flags, icmp_type, load_len = None, None, None, None, None, None, None, None, None
+		protocol = []
+
+		if len(self.RECENT_ACTIVITIES)>5:
+			self.RECENT_ACTIVITIES = self.RECENT_ACTIVITIES[-5:]
+		
+		for category in self.FILTERED_ACTIVITIES:
+			if len(self.FILTERED_ACTIVITIES[category]['activities'])>30:
+				self.FILTERED_ACTIVITIES[category]['activities'] = self.FILTERED_ACTIVITIES[category]['activities'][-30:]
+
+		self.set_flags()
+
+		if Ether in pkt:
+			src_mac = pkt[Ether].src
+			dst_mac = pkt[Ether].dst
+		elif IP in pkt:
+			src_ip = pkt[IP].src
+			dst_ip = pkt[IP].dst
+		
+		if TCP in pkt:
+			protocol.append("TCP")
+			src_port = pkt[TCP].sport
+			dst_port = pkt[TCP].dport
+			tcp_flags = pkt[TCP].flags.flagrepr()
+		elif UDP in pkt:
+			protocol.append("UDP")
+			src_port = pkt[UDP].sport
+			dst_port = pkt[UDP].dport
+		elif ICMP in pkt:
+			protocol.append("ICMP")
+			icmp_type = pkt[ICMP].type # 8 for echo-request and 0 for echo-reply
+		
+		if ARP in pkt and pkt[ARP].op in (1,2):
+			protocol.append("ARP")
+			if pkt[ARP].hwsrc in self.MAC_TABLE.keys() and self.MAC_TABLE[pkt[ARP].hwsrc] != pkt[ARP].psrc:
+				self.MAC_TABLE[pkt[ARP].hwsrc] = pkt[ARP].psrc
+			if pkt[ARP].hwsrc not in self.MAC_TABLE.keys():
+				self.MAC_TABLE[pkt[ARP].hwsrc] = pkt[ARP].psrc
+
+		if Raw in pkt:
+			load_len = len(pkt[Raw].load)
+		
+		if ICMP in pkt:
+			if src_ip == MY_IP and src_mac != MY_MAC:
+				# self.FILTERED_ACTIVITIES['ICMP-SMURF']['activities'].append([pkt.time, icmp_type, src_ip, src_mac, dst_ip, dst_mac, load_len])
+				self.FILTERED_ACTIVITIES['ICMP-SMURF']['activities'].append([pkt.time])
+
+			if load_len>1024:
+				# self.FILTERED_ACTIVITIES['ICMP-POD']['activities'].append([pkt.time, icmp_type, src_ip, src_mac, dst_ip, dst_mac, load_len])
+				self.FILTERED_ACTIVITIES['ICMP-POD']['activities'].append([pkt.time])
+
+		if dst_ip == MY_IP:
+			if TCP in pkt:
+				if tcp_flags == "S":
+					# self.FILTERED_ACTIVITIES['TCP-SYN']['activities'].append([pkt.time, src_ip, src_port, src_mac, dst_ip, dst_port, dst_mac, load_len])
+					self.FILTERED_ACTIVITIES['TCP-SYN']['activities'].append([pkt.time])
+
+				elif tcp_flags == "SA":
+					# self.FILTERED_ACTIVITIES['TCP-SYNACK']['activities'].append([pkt.time, src_ip, src_port, src_mac, dst_ip, dst_port, dst_mac, load_len])
+					self.FILTERED_ACTIVITIES['TCP-SYNACK']['activities'].append([pkt.time])
+
+		self.RECENT_ACTIVITIES.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len])
+
 def clear_screen():
 	if "linux" in sys.platform:
 		os.system("clear")
@@ -19,41 +133,6 @@ def clear_screen():
 		os.system("cls")
 	else:
 		pass
-
-def sniffer_threader():
-	while True:
-		pkt = sniff(count=1)
-		with threading.Lock():
-			PACKETS_QUEUE.put(pkt[0])
-
-def analyze_threader():
-	while True:
-		pkt = PACKETS_QUEUE.get()
-		analyze_packet(pkt)
-		PACKETS_QUEUE.task_done()
-
-def find_attackers(mac_data):
-	msg = []
-	for mac in mac_data:
-		msg.append("["+str(mac_table[mac])+" ("+mac+")]" if mac in mac_table else "[Unknown IP ("+mac+")]")
-	return " ".join(msg)
-
-def check_avg_time(activities):
-	time = 0
-	c = -1
-	while c>-21:
-		time += activities[c][0] - activities[c-1][0]
-		c -= 1
-	time /= len(activities)
-	return ( time<2 and recent_activities[-1][0] - activities[-1][0] < 10)
-
-def set_flags():
-	global mac_table, recent_activities, filtered_activities, PACKETS_QUEUE, INTERFACE, MY_NETMASK, MY_IP, MY_MAC
-	for category in filtered_activities:
-		if len(filtered_activities[category]['activities'])>20:
-			filtered_activities[category]['flag'] = check_avg_time(filtered_activities[category]['activities'])
-			if filtered_activities[category]['flag']:
-				filtered_activities[category]['attacker-mac'] = list(set([i[3] for i in filtered_activities[category]['activities']]))
 	
 def is_admin():
 	try:
@@ -64,67 +143,6 @@ def is_admin():
 		return ctypes.windll.shell32.IsUserAnAdmin() == 1
 	except AttributeError:
 		return False
-		
-def analyze_packet(pkt):
-	global mac_table, recent_activities, filtered_activities, PACKETS_QUEUE, INTERFACE, MY_NETMASK, MY_IP, MY_MAC
-	src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, tcp_flags, icmp_type, load_len = None, None, None, None, None, None, None, None, None
-	protocol = []
-
-	if len(recent_activities)>5:
-		recent_activities = recent_activities[-5:]
-	
-	for category in filtered_activities:
-		if len(filtered_activities[category]['activities'])>30:
-			filtered_activities[category]['activities'] = filtered_activities[category]['activities'][-30:]
-
-	set_flags()
-
-	if Ether in pkt:
-		src_mac = pkt[Ether].src
-		dst_mac = pkt[Ether].dst
-	elif IP in pkt:
-		src_ip = pkt[IP].src
-		dst_ip = pkt[IP].dst
-	
-	if TCP in pkt:
-		protocol.append("TCP")
-		src_port = pkt[TCP].sport
-		dst_port = pkt[TCP].dport
-		tcp_flags = pkt[TCP].flags.flagrepr()
-	elif UDP in pkt:
-		protocol.append("UDP")
-		src_port = pkt[UDP].sport
-		dst_port = pkt[UDP].dport
-	elif ICMP in pkt:
-		protocol.append("ICMP")
-		icmp_type = pkt[ICMP].type # 8 for echo-request and 0 for echo-reply
-	
-	if ARP in pkt and pkt[ARP].op in (1,2):
-		protocol.append("ARP")
-		if pkt[ARP].hwsrc in mac_table.keys() and mac_table[pkt[ARP].hwsrc] != pkt[ARP].psrc:
-			mac_table[pkt[ARP].hwsrc] = pkt[ARP].psrc
-		if pkt[ARP].hwsrc not in mac_table.keys():
-			mac_table[pkt[ARP].hwsrc] = pkt[ARP].psrc
-
-	if Raw in pkt:
-		load_len = len(pkt[Raw].load)
-	
-	if ICMP in pkt:
-		if src_ip == MY_IP and src_mac != MY_MAC:
-			filtered_activities['ICMP-SMURF']['activities'].append([pkt.time, icmp_type, src_ip, src_mac, dst_ip, dst_mac, load_len])
-			recent_activities.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len, "<ICMP SMURF PACKET>"])
-		if load_len>1024:
-			filtered_activities['ICMP-POD']['activities'].append([pkt.time, icmp_type, src_ip, src_mac, dst_ip, dst_mac, load_len])
-			recent_activities.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len, "<PING OF DEATH PACKET>"])
-	if dst_ip == MY_IP:
-		if TCP in pkt:
-			if tcp_flags == "S":
-				filtered_activities['TCP-SYN']['activities'].append([pkt.time, src_ip, src_port, src_mac, dst_ip, dst_port, dst_mac, load_len])
-				recent_activities.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len, "<SYN PACKET>"])
-			elif tcp_flags == "SA":
-				filtered_activities['TCP-SYNACK']['activities'].append([pkt.time, src_ip, src_port, src_mac, dst_ip, dst_port, dst_mac, load_len])
-				recent_activities.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len, "<SYN-ACK PACKET>"])
-	recent_activities.append([pkt.time, protocol, src_ip, dst_ip, src_mac, dst_mac, src_port, dst_port, load_len, None])
 
 def main():
 	global mac_table, recent_activities, filtered_activities, PACKETS_QUEUE, INTERFACE, MY_NETMASK, MY_IP, MY_MAC
